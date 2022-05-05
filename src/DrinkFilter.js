578bef2bef48e2f5f4c78e7743a88c8becfa8097
Owl200 <alromaru@hotmail.com>
2022-05-05 10:07:06 -0500

    now lists display items by 10 and clicking on a list item name will show the full drink info

--- src/DrinkFilter.js
+++ src/DrinkFilter.js
@@ -5,44 +5,39 @@ import MiniDrink from './MiniDrink.js'
 export default function DrinkFilter(){
     const [firstDdValue, setFirstDdValue] = useState('random') // will decide the case for the first drop-down
     const [ingredientsArr, setIngredientArr] = useState([]) // value that will be populate the second drop-down
-    const [ingredientDd, setIngredientDd] = useState('')  // value gotten from the second drop-down
+    const [ingredientDd, setIngredientDd] = useState('7-up')  // value gotten from the second drop-down
     const [drink, setDrink] = useState({}) // object including all the info for a drink
     const [drinkName, setDrinkName] = useState('Tequila sunrise') // value used to search for a drink by name
     const [drinkMini, setDrinkMini] = useState([{strDrink: 'Tequila SunRise', strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/quqyqp1480879103.jpg'}])
-    const [indexStart, setIndexStart] =useState(0) // sets the index where the drinkMiniArr will start at
+    const [indexStart, setIndexStart] = useState(0) // sets the index where the drinkMiniArr will start at
+    const [drinkMiniArr, setDrinkMiniArr] = useState([]) 
+    const [drinkArrButtons, setDrinkArrButtons] = useState([])
 
-    // used for the first drop-down
+
+    // used to sort the ingredient list on the second dropdown
     useEffect( _ =>  {
-        switch (firstDdValue) {
-            case 'ingredient':
-                fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list')
-                .then(res => res.json())
-                .then(srt => srt.drinks.sort((a, b) => {
-                    if (a.strIngredient1 < b.strIngredient1) {
-                        return -1;
-                    } else if (b.strIngredient1 < a.strIngredient1) {
-                        return 1;
-                    } else {
-                        return 0;
-                    }
-                }))
-                .then(data => setIngredientArr(data.map( (item, i) => (
-                <option value={item.strIngredient1.toLowerCase().replaceAll(' ', '-')} key={i}>{item.strIngredient1}</option>))));
-                break;
-            case 'random':
-                break;
-            default:
-                break;
-        }
+        firstDdValue === 'ingredient' && fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list')
+        .then(res => res.json())
+        .then(srt => srt.drinks.sort((a, b) => {
+            if (a.strIngredient1 < b.strIngredient1) {
+                return -1;
+            } else if (b.strIngredient1 < a.strIngredient1) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }))
+        .then(data => setIngredientArr(data.map( (item, i) => (
+        <option value={item.strIngredient1.toLowerCase().replaceAll(' ', '-')} key={i}>{item.strIngredient1}</option>))));
     }, [firstDdValue])
     
     // used to get a Drink info by name after drinkName changes
     useEffect( _ => {
-        firstDdValue === 'ingredient' && fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drinkName}`)
+        fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drinkName}`)
         .then(res => res.json())
         .then(data => setDrink(data.drinks[0])) 
         .catch(err => console.log(err))
-    }, [drinkName, firstDdValue] )
+    }, [drinkName] )
 
     // To run when the page first loads 
     useEffect( _ => {
@@ -58,9 +53,60 @@ export default function DrinkFilter(){
         `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredientDd.replaceAll(' ', '-')}` :
         `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredientDd.replaceAll('-', ' ')}`)
         .then(res => res.json())
-        .then(data => setDrinkMini(data.drinks))
-        .catch(e => console.log(e))
+        .then(srt => srt.drinks.sort((a, b) => {
+            if (a.strIngredient1 < b.strIngredient1) {
+                return -1;
+            } else if (b.strIngredient1 < a.strIngredient1) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }))
+        .then(data => setDrinkMini(data))
+        .catch(e => console.log(e));
+        setIndexStart(0)
     }, [ingredientDd, firstDdValue])
+    
+    useEffect( _ => {
+        if(drinkMini.length <= 10) {
+            setDrinkMiniArr( drinkMini.map( (element, i) => {
+            if(drinkName.toLowerCase() === element.strDrink.toLowerCase()) {
+                return <Drink drink={drink}/>
+            } else {
+                return <MiniDrink setDrinkName={setDrinkName} drink={element} drinkId={i}/>
+            }}))
+            setDrinkArrButtons([])
+        } else {
+            setDrinkMiniArr([])
+            setDrinkArrButtons([])
+            for(let i = 0; i < Math.ceil(drinkMini.length/10); i++){
+                setDrinkArrButtons(prev => [...prev, <button onClick={arrayButton}>{i + 1}</button>])
+            }
+            if(drinkMini.slice(indexStart).length < 10) {
+                setDrinkMiniArr(drinkMini.slice(indexStart).map( (element, i) => {
+                    if(drinkName.toLowerCase() === element.strDrink.toLowerCase()) {
+                        return <Drink drink={drink}/>
+                    } else {
+                        return <MiniDrink setDrinkName={setDrinkName} drink={element} drinkId={i}/>
+                    }
+                }) )
+            } else {
+                for(let i = indexStart; i <= indexStart + 9; i++) {
+                    if(drinkMini[i].strDrink.toLowerCase() === drinkName.toLowerCase()){
+                        setDrinkMiniArr(prev => [...prev, <Drink drink={drink} />])
+                    } else{ 
+                    setDrinkMiniArr(prev => [...prev, <MiniDrink setDrinkName={setDrinkName} drink={drinkMini[i]} drinkId={i}/>])
+                    }
+                }
+            }
+        }
+    }, [drinkMini, indexStart, drinkName, drink])
+
+    console.log(drinkName)
+
+    //event handler for the buttons that appear on the drinks list 
+    const arrayButton = e => setIndexStart((+e.target.innerHTML - 1) * 10)
+
 
     // used to get the value from the first drop-down 
     const handleChangeSearch = e => setFirstDdValue(e.target.value);  
@@ -81,24 +127,14 @@ export default function DrinkFilter(){
         .catch(err => console.log(err));
     }
 
-    // onClick for button that appears when searching by name
-    const getDrinkByName = _ => {
-        fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drinkName}`)
-        .then(res => res.json())
-        .then(data => setDrink(data.drinks[0]))
-        .catch(err => console.log(err));
-    }
-
     // sets the value of the input when searching by name 
     const handleChangeName = e => {
         setDrinkName(e.target.value);
     }
 
-    let drinkMiniArr;
-    ingredientDd && (drinkMiniArr = drinkMini.map( (element, i) => <MiniDrink drink={element} drinkId={i}/>))
-    
+    // used to select the all the text inside the input when clicking on it 
+    const handleFocus = e => e.target.select()
 
-    console.log(drinkMini)
     return(
         <>
             <header>
@@ -118,15 +154,17 @@ export default function DrinkFilter(){
                 </select>}
 
                 {firstDdValue === 'random' && <button onClick={getRandom}>Get a random drink</button>}
-                {firstDdValue === 'name' && 
-                <>
-                    <input value={drinkName} className='filter' onChange={handleChangeName}></input>
-                    <button onClick={getDrinkByName}>Search</button>
-                </>}
+                {firstDdValue === 'name' && <input onClick={handleFocus} value={drinkName} className='filter' onChange={handleChangeName}></input>}
             </div>
 
             {firstDdValue === 'ingredient' || <Drink drink={drink}/>}
+            {firstDdValue === 'ingredient' && <p style={{textAlign: 'center'}}>{drinkMini.length} results found</p>}
             {firstDdValue === 'ingredient' && drinkMiniArr}
+            {firstDdValue === 'ingredient' && (
+                <div className='drink-arr-buttons'>
+                    {drinkArrButtons}
+                </div>)
+                }
         </>
     )
 }
--- src/MiniDrink.js
+++ src/MiniDrink.js
@@ -4,19 +4,19 @@ import './mini-drink.css'
 export default function MiniDrink(props){
     
     //console.log(props)
-    const handleClick = e => console.log(e.target) 
+    const handleClick = e => props.setDrinkName(e.target.innerText) 
     
 
     return(
         <div className='main'>
-            <div onClick={handleClick} className='wrapper-mini' key={props.drinkId}>
+            <div  className='wrapper-mini' key={props.drinkId}>
                 <div className='image-grid-mini'>
                     <div className='image'>
                         <img  src={props.drink.strDrinkThumb} alt="" />
                     </div>
                 </div>
                 <div className='drink-name-mini'>
-                    <h2>{props.drink.strDrink}</h2>
+                    <h2 onClick={handleClick}>{props.drink.strDrink}</h2>
                 </div>
             </div>
         </div>
