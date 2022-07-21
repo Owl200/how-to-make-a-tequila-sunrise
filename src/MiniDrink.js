7ef4ab17acd0fd7b010ca4ad020a94c3cad4f83d
Owl200 <alromaru@hotmail.com>
2022-07-20 22:28:18 -0500

    modified MiniDrink so that the whole component is clickable

--- src/MiniDrink.js
+++ src/MiniDrink.js
@@ -1,23 +1,23 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import './mini-drink.css'
 
 export default function MiniDrink(props){
     
-    const handleClick = e => props.setDrinkName(e.target.innerText) 
-    
+    const handleClick = e => props.setDrinkName(drinkValue.current.name) 
+    const drinkValue = useRef(null)
 
     return(
         <div className='main'>
-            <div  className='wrapper-mini' key={props.drinkId}>
+            <button ref={drinkValue} className='wrapper-mini' key={props.drinkId} onClick={handleClick} name={props.drink.strDrink}>
                 <div className='image-grid-mini'>
                     <div className='image'>
                         <img  src={props.drink.strDrinkThumb} alt="" />
                     </div>
                 </div>
                 <div className='drink-name-mini'>
-                    <h2 onClick={handleClick}>{props.drink.strDrink}</h2>
+                    <h2 >{props.drink.strDrink}</h2>
                 </div>
-            </div>
+            </button>
         </div>
     )
 }
